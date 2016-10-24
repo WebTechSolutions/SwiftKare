08cfe85dedd4dbacc44ec3e02269c5cc66eaf1dd
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-10-24 08:27:53 +0500

    add irepository

---
+++ WebApp/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApp.Interfaces
+{
+    public interface IRepository<T> where T : new()
+    {
+        IQueryable<T> GetList();
+        T GetById(long id);
+        T Put(long id, T t);
+        T Add(T t);
+        void Delete(int id);
+        T Find(object id);
+        bool Exists(object id);
+    }
+}
