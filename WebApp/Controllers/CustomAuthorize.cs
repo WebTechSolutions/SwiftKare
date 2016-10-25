c4cad7575d0af5e008b6e0228ed19805a09390d5
naveed-khan <meetmeonly@gmail.com>
2016-10-25 23:03:55 +0500

    sdsdsdsdsdsd

---
+++ WebApp/Controllers/CustomAuthorize.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApp
+{
+    public class CustomAuthorize : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                //base.HandleUnauthorizedRequest(filterContext);
+                filterContext.Result = new RedirectToRouteResult(new
+                RouteValueDictionary(new { controller = "AdminLogin", action = "AdminLogin" }));
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                RouteValueDictionary(new { controller = "AdminLogin", action = "AdminLogin" }));
+            }
+        }
+    }
+}
