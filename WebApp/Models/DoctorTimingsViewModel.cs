a712c53516a8192a09f3acdf6ebf0173a52d1e25
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-10-26 16:56:49 +0500

    adding doctortimingsviewmodel

---
+++ WebApp/Models/DoctorTimingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebApp.Models
+{
+    public class DoctorTimingsViewModel
+    {
+        public IEnumerable<DoctorTiming> DoctorTimingsList { get; set; }
+        public DoctorTiming DoctorTiming { get; set; }
+        public Timing Timing { get; set; } = new Timing();
+        public IEnumerable<SelectListItem> Days { get; set; }
+    }
+
+    public class Timing
+    {
+        public string From { get; set; } = "";
+        public string To { get; set; } = "";
+    }
+
+}
