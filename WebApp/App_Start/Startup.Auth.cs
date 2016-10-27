3753fed2e9d386a480e90e7c5c1012c68bda192b
naveed-khan <meetmeonly@gmail.com>
2016-10-27 08:08:27 +0500

    was getting Owin Exception on project startup so I added this line  OnException = context => { } to this file.

--- WebApp/App_Start/Startup.Auth.cs
+++ WebApp/App_Start/Startup.Auth.cs
@@ -34,7 +34,9 @@ namespace WebApp
                     // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
                         validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager)),
+                         //**** This what I did ***//
+                        OnException = context => { }
                 }
             });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
