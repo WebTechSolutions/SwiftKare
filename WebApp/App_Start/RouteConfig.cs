5fe4094ed2cdb0a59d080dc234ef75d78d8bfb80
naveed-khan <meetmeonly@gmail.com>
2016-10-27 08:07:22 +0500

    to make one login page for both admin and doctor and patient

--- WebApp/App_Start/RouteConfig.cs
+++ WebApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "AdminLogin", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
