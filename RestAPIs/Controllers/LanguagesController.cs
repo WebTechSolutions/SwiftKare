01c9dce7d7411e59fee9ebbcfc4bf8c0e67931ea
naveed-khan <meetmeonly@gmail.com>
2016-10-28 22:28:26 +0500

    new apis for langauage and speciality

---
+++ RestAPIs/Controllers/LanguagesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DataAccess;
+
+using System.Collections;
+
+namespace RestAPIs.Controllers
+{
+    public class LanguagesController : ApiController
+    {
+        private SwiftKareDBEntities db = new SwiftKareDBEntities();
+
+        // GET: api/Languages
+        public IQueryable<Language> GetLanguages()
+        {
+            return db.Languages;
+        }
+            
+            
+        // GET: api/Languages/5
+        [ResponseType(typeof(Language))]
+        public async Task<IHttpActionResult> GetLanguage(long id)
+        {
+            Language language = await db.Languages.FindAsync(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(language);
+        }
+
+        // PUT: api/Languages/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutLanguage(long id, Language language)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != language.languageID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(language).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LanguageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Languages
+        [ResponseType(typeof(Language))]
+        public async Task<IHttpActionResult> PostLanguage(Language language)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Languages.Add(language);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = language.languageID }, language);
+        }
+
+        // DELETE: api/Languages/5
+        [ResponseType(typeof(Language))]
+        public async Task<IHttpActionResult> DeleteLanguage(long id)
+        {
+            Language language = await db.Languages.FindAsync(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            db.Languages.Remove(language);
+            await db.SaveChangesAsync();
+
+            return Ok(language);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LanguageExists(long id)
+        {
+            return db.Languages.Count(e => e.languageID == id) > 0;
+        }
+
+        private IEnumerable<Language> Get()
+        {
+            return db.Languages.ToList();
+        }
+    }
+}
---
+++ RestAPIs/Controllers/SpeciallitiesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DataAccess;
+
+namespace RestAPIs.Controllers
+{
+    public class SpeciallitiesController : ApiController
+    {
+        private SwiftKareDBEntities db = new SwiftKareDBEntities();
+
+        // GET: api/Speciallities
+        public IQueryable<Speciallity> GetSpeciallities()
+        {
+            return db.Speciallities;
+        }
+        private IEnumerable<Speciallity> Get()
+        {
+            return db.Speciallities.ToList();
+        }
+        // GET: api/Speciallities/5
+        [ResponseType(typeof(Speciallity))]
+        public async Task<IHttpActionResult> GetSpeciallity(long id)
+        {
+            Speciallity speciallity = await db.Speciallities.FindAsync(id);
+            if (speciallity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(speciallity);
+        }
+
+        // PUT: api/Speciallities/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSpeciallity(long id, Speciallity speciallity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != speciallity.speciallityID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(speciallity).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SpeciallityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Speciallities
+        [ResponseType(typeof(Speciallity))]
+        public async Task<IHttpActionResult> PostSpeciallity(Speciallity speciallity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Speciallities.Add(speciallity);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = speciallity.speciallityID }, speciallity);
+        }
+
+        // DELETE: api/Speciallities/5
+        [ResponseType(typeof(Speciallity))]
+        public async Task<IHttpActionResult> DeleteSpeciallity(long id)
+        {
+            Speciallity speciallity = await db.Speciallities.FindAsync(id);
+            if (speciallity == null)
+            {
+                return NotFound();
+            }
+
+            db.Speciallities.Remove(speciallity);
+            await db.SaveChangesAsync();
+
+            return Ok(speciallity);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SpeciallityExists(long id)
+        {
+            return db.Speciallities.Count(e => e.speciallityID == id) > 0;
+        }
+    }
+}
