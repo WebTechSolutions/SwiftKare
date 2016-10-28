8c538c30d9b7fbe946386eb65b76c12668fdec3d
naveed-khan <meetmeonly@gmail.com>
2016-10-28 22:21:46 +0500

    new repositories

---
+++ WebApp/Repositories/DoctorRepositories/LanguageRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using WebApp.Interfaces;
+using Newtonsoft.Json;
+using WebApp.Helper;
+using Identity.Membership.Models;
+using System.Collections;
+
+namespace WebApp.Repositories.DoctorRepositories
+{
+   
+
+   
+        public class LanguageRepository : IRepository<Language>
+    {
+
+        public Language Add(Language t)
+        {
+           
+            var strContent = JsonConvert.SerializeObject(t);
+            var response = ApiConsumerHelper.PostData("api/Languages", strContent);
+            var result = JsonConvert.DeserializeObject<Language>(response);
+           
+            return result;
+        }
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerable<Language> Get()
+        {
+            var response = ApiConsumerHelper.GetResponseString("api/Languages");
+            var result = JsonConvert.DeserializeObject<List<Language>>(response);
+           // var result = JsonConvert.DeserializeObject<Language>(response);
+            return result;
+        }
+        public IQueryable<Language> GetLanguages()
+        {
+            throw new NotImplementedException();
+        }
+       
+
+        public bool Exists(object id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Language Find(object id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Language GetById(long id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Language GetByUserId(string userId)
+            {
+                var response = ApiConsumerHelper.GetResponseString("api/Languages?userId=" + userId);
+                var result = JsonConvert.DeserializeObject<Language>(response);
+                return result;
+            }
+
+        public IQueryable<Language> GetLanguages(long langId)
+        {
+            var response = ApiConsumerHelper.GetResponseString("api/Languages?languageId=" + langId);
+            var result = JsonConvert.DeserializeObject<IQueryable<Language>>(response);
+            return result;
+        }
+
+        public Language Put(long id, Language t)
+            {
+                throw new NotImplementedException();
+            }
+
+               
+    }
+    }
+
---
+++ WebApp/Repositories/DoctorRepositories/SpecialityRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using WebApp.Interfaces;
+using Newtonsoft.Json;
+using WebApp.Helper;
+using Identity.Membership.Models;
+
+namespace WebApp.Repositories.DoctorRepositories
+{
+    public class SpecialityRepository : IRepository<Speciallity>
+    {
+
+        public Speciallity Add(Speciallity t)
+        {
+            
+            var strContent = JsonConvert.SerializeObject(t);
+            var response = ApiConsumerHelper.PostData("api/Speciallities", strContent);
+            var result = JsonConvert.DeserializeObject<Speciallity>(response);
+
+            return result;
+        }
+      
+        public IQueryable<Speciallity> GetSpeciallities()
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Exists(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Speciallity Find(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Speciallity GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Speciallity> Get()
+        {
+            var response = ApiConsumerHelper.GetResponseString("api/Speciallities");
+            var result = JsonConvert.DeserializeObject<List<Speciallity>>(response);
+            // var result = JsonConvert.DeserializeObject<Language>(response);
+            return result;
+        }
+
+        public Speciallity Put(long id, Speciallity t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
