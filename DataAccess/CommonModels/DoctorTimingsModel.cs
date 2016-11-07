19f49047d85b287c8cb3b3545192e4b63f5ce413
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-07 18:26:57 +0500

    commit and push

---
+++ DataAccess/CommonModels/DoctorTimingsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataAccess.CommonModels
+{
+    public class DoctorTimingsModel
+    {
+        public long doctorTimingsID { get; set; }
+        public long doctorID { get; set; }
+        public string day { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public string cb { get; set; }
+        public string mb { get; set; }
+        public Nullable<System.DateTime> cd { get; set; }
+        public Nullable<System.DateTime> md { get; set; }
+        public Nullable<bool> active { get; set; }
+    }
+}
