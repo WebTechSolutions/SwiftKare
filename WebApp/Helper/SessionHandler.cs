87b4eef58e222ca732cc62ef954bc7c594dea8a2
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-12 07:00:48 +0500

    commit

---
+++ WebApp/Helper/SessionHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Web;
+
+namespace WebApp.Helper
+{
+    public static class SessionHandler
+    {
+        private static string username;  
+        public static string UserName    
+        {
+            get
+            {
+                return HttpContext.Current.Session["username"].ToString();
+            }
+            set
+            {
+                HttpContext.Current.Session["username"] = value;
+                username = value;
+
+            }
+        }
+
+
+        private static string password;
+        public static string Password
+        {
+            get
+            {
+                return HttpContext.Current.Session["password"].ToString();
+            }
+            set
+            {
+                HttpContext.Current.Session["password"] = value;
+                password = value;
+
+            }
+        }
+
+        private static string userId;
+        public static string UserId
+        {
+            get
+            {
+                return HttpContext.Current.Session["userId"].ToString();
+            }
+            set
+            {
+                HttpContext.Current.Session["userId"] = value;
+                userId = value;
+
+            }
+        }
+
+
+    }
+}
