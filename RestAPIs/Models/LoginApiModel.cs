606701d16057a99ac6b7d0e59162a97916327738
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-13 04:04:57 +0500

    add login api model

---
+++ RestAPIs/Models/LoginApiModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestAPIs.Models
+{
+    public class LoginApiModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
