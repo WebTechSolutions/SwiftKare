37ac3a13195eb4869fdf30e06f11d8438c2e9780
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-13 04:05:24 +0500

    add register api model

---
+++ RestAPIs/Models/RegisterApiModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RestAPIs.Models
+{
+    public class RegisterApiModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Role { get; set; }
+    }
+}
