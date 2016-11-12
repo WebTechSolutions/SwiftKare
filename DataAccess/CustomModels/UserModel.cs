ac138b47ffff3193cfa961baa2ceb17c0a4e0cd4
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-12 23:18:42 +0500

    add the user model for api communication

---
+++ DataAccess/CustomModels/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccess.CustomModels
+{
+    public class UserModel
+    {
+        public long Id { get; set; }
+        public string LastName { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string Email { get; set; } = "";
+    }
+}
