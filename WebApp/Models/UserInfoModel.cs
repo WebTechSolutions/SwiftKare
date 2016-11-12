07be9c07a838466502cd4523cd9bfbe3bb92c6db
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-12 23:18:00 +0500

    add userinfo model in web app level

---
+++ WebApp/Models/UserInfoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Models
+{
+    public class UserInfoModel
+    {
+        public long Id { get; set; }
+        public string LastName { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string Email { get; set; } = "";
+    }
+
+}
