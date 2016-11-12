261b85180f4ed251b1beba214bb0f2416fdc235f
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-12 23:19:21 +0500

    add user model in session handler class

--- WebApp/Helper/SessionHandler.cs
+++ WebApp/Helper/SessionHandler.cs
@@ -1,55 +1,55 @@
 ﻿using System.Web;
-
+using WebApp.Models;
 namespace WebApp.Helper
 {
     public static class SessionHandler
     {
-        private static string username;  
-        public static string UserName    
+        public static string UserName
         {
             get
             {
-                return HttpContext.Current.Session["username"].ToString();
+
+                return HttpContext.Current.Session["username"] != null ? HttpContext.Current.Session["username"].ToString() : "";
             }
             set
             {
                 HttpContext.Current.Session["username"] = value;
-                username = value;
-
             }
         }
-
-
-        private static string password;
         public static string Password
         {
             get
             {
-                return HttpContext.Current.Session["password"].ToString();
+                return HttpContext.Current.Session["password"]!=null? HttpContext.Current.Session["password"].ToString():"";
             }
             set
             {
                 HttpContext.Current.Session["password"] = value;
-                password = value;
-
             }
         }
-
-        private static string userId;
         public static string UserId
         {
             get
             {
-                return HttpContext.Current.Session["userId"].ToString();
+                return HttpContext.Current.Session["userId"]!=null?HttpContext.Current.Session["userId"].ToString():"";
             }
             set
             {
                 HttpContext.Current.Session["userId"] = value;
-                userId = value;
-
             }
         }
 
-
+        public static UserInfoModel UserInfo
+        {
+            get
+            {
+                return HttpContext.Current.Session["UserInfo"] != null ? (UserInfoModel)HttpContext.Current.Session["UserInfo"] : new UserInfoModel();
+            }
+            set
+            {
+                HttpContext.Current.Session["UserInfo"] = value;
+            }
+        }
     }
+
 }
