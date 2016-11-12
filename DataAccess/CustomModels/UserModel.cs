52a13bae5f6c935ed98b7c49d488463f2524eeab
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-13 03:59:23 +0500

    add the errors list

--- DataAccess/CustomModels/UserModel.cs
+++ DataAccess/CustomModels/UserModel.cs
@@ -1,4 +1,6 @@
-﻿namespace DataAccess.CustomModels
+﻿using System.Collections.Generic;
+
+namespace DataAccess.CustomModels
 {
     public class UserModel
     {
@@ -6,6 +8,6 @@
         public string LastName { get; set; } = "";
         public string FirstName { get; set; } = "";
         public string Email { get; set; } = "";
-        public string ErrorMessage { get; set; } = "";
+        public List<string> Errors { get; set; }
     }
 }
