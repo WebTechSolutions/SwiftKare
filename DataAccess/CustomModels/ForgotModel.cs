5f02037b9541b8372bf7437acc5cbc4d3d49239f
shujaatgul <it.engr_shujaatgul@yahoo.com>
2016-11-14 03:23:41 +0500

    commiting forgot & reset api

---
+++ DataAccess/CustomModels/ForgotModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.CustomModels
+{
+    public class ForgotModel
+    {
+        public string Email { get; set; }
+        public string SecretQuestion { get; set; }
+        public string SecretAnswer { get; set; }
+    }
+}
