61bccd8feae4fdd21f996df3027adc5da2002af1
naveed-khan <meetmeonly@gmail.com>
2016-11-17 19:51:53 +0500

    allergy custom model

---
+++ DataAccess/CustomModels/AllergiesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CustomModels
+{
+    public class AllergiesModel
+    {
+        public long allergyID { get; set; }
+        public string allergyName { get; set; }
+    }
+}
