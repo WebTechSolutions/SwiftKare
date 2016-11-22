fa1c49685743bf28a3f2e6a69d15e5946b7ccc54
naveed-khan <meetmeonly@gmail.com>
2016-11-23 03:22:38 +0500

    copied from new template

---
+++ WebApp/Content/css/abc.php
@@ -0,0 +1,311 @@
+  
+<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          Subjective
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in">
+      <div class="panel-body">
+        <textarea class="form-control" rows="1"></textarea>
+      </div>  
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          Objective
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        <textarea class="form-control" rows="1"></textarea>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          Assessment
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        <textarea class="form-control" rows="1"></textarea>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+          Plan
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFour" class="panel-collapse collapse">
+      <div class="panel-body">
+        <textarea class="form-control" rows="1"></textarea>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">
+          Review of Systems
+        </a>
+      </h4>
+    </div>
+    <div id="collapseFive" class="panel-collapse collapse">
+      <div class="panel-body nano" id="about">
+                        <div class="content">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td style="border-top: 0">
+                                            <h4 class="m-0">General</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; fever</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; chills</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; malaise</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; weight loss</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; weight gain</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Head, Earn, Nose, Throat</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; eye pain</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; vision change</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; ear pain</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; discharge</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; hearing change</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; nasal congestion</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Heart</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; chest pain</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; SOB</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; palpitations</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; dizziness</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Respiratory</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; cough</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; wheezing</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">GI</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; abd pain</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; nausea</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; emesis</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; diarrhea</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Musculoskeletal</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; arthritis</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; back pain</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; muscle pain</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; cramps</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; weakness</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">skin</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; rash or ulcers</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Psychiatry</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; anxiety</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; depression</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Hematology</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; bleeding problems</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+
+                                    <tr>
+                                        <td>
+                                            <h4 class="m-0">Nuerology</h4>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input checked="checked" class="flat" type="checkbox">&nbsp; Siezure</label>
+                                            </div>
+
+
+                                            <div class="checkbox" style="display: inline-block; width: 150px;">
+                                                <label><input class="flat" type="checkbox">&nbsp; focal weakness</label>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+    </div>
+  </div>
+
+</div>
