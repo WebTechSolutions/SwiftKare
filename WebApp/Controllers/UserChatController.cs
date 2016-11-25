983d7acbd3268de2b0509de432b34f462495a264
Hiren Patel <nlightech@gmail.com>
2016-11-25 18:49:57 +0530

    User Chat: Implemented  Demo.

--- WebApp/Controllers/UserChatController.cs
+++ WebApp/Controllers/UserChatController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApp.Helper;
 
 namespace WebApp.Controllers
 {
@@ -11,6 +12,16 @@ namespace WebApp.Controllers
         // GET: UserChat
         public ActionResult Index()
         {
+            if(HttpContext.Application["OpenTokSession"] == null)
+            {
+                HttpContext.Application["OpenTokSession"] = UserChatHelper.GenerateOpenTokSession();
+                HttpContext.Application["OpenTokToken"] = UserChatHelper.GenerateOpenTokToken(Convert.ToString(HttpContext.Application["OpenTokSession"]));
+            }
+
+            ViewBag.OpenTokApiKey = UserChatHelper.TokBoxApiKey;
+            ViewBag.OpenTokSession = Convert.ToString(HttpContext.Application["OpenTokSession"]);
+            ViewBag.OpenTokToken = Convert.ToString(HttpContext.Application["OpenTokToken"]);
+
             return View();
         }
     }
---
+++ WebApp/Helper/UserChatHelper.cs
@@ -0,0 +1,41 @@
+﻿using OpenTokSDK;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Helper
+{
+    public class UserChatHelper
+    {
+
+        #region Declarations
+
+        public static int TokBoxApiKey = Convert.ToInt32(ConfigurationManager.AppSettings["TokBoxApiKey"].ToString());
+        private static string TokBoxSecretKey = ConfigurationManager.AppSettings["TokBoxSecretKey"].ToString();
+
+
+        #endregion
+
+        #region Public Methods
+
+
+        public static string GenerateOpenTokSession()
+        {
+            var openTok = new OpenTok(TokBoxApiKey, TokBoxSecretKey);
+            return openTok.CreateSession(mediaMode: MediaMode.RELAYED).Id;
+        }
+
+        public static string GenerateOpenTokToken(string sessionId)
+        {
+            var openTok = new OpenTok(TokBoxApiKey, TokBoxSecretKey);
+
+            //By default token is valid for 24 hours. So does not need to modify it
+            return openTok.GenerateToken(sessionId);
+        }
+
+        #endregion
+
+    }
+}
