d6e8e349cbaaedd0f6f0cac9740fe64857d4281b
Hiren Patel <nlightech@gmail.com>
2016-11-25 11:44:52 +0530

    VideoChat: Created intial controller and incorporated design.

---
+++ WebApp/Controllers/UserChatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class UserChatController : Controller
+    {
+        // GET: UserChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
