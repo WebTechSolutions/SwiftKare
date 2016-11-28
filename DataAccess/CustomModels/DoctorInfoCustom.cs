1a909bc8677083c129c75bf8a3acf3bcc6615a31
naveed-khan <meetmeonly@gmail.com>
2016-11-29 00:08:26 +0500

    see doctor module update new css and js and models

---
+++ DataAccess/CustomModels/DoctorInfoCustom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CustomModels
+{
+    public class DoctorInfoCustom
+    {
+        public long doctorID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string cellPhone { get; set; }
+        public string specialityName { get; set; }
+       
+        
+    }
+}
