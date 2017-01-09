c7bb2cd37ed16dae75caa8e3892fd6a048fea02d
Hiren Patel <nlightech@gmail.com>
2017-01-09 18:23:02 +0530

    DoseSpot Demo.

---
+++ DataAccess/CustomModels/DoseSpotModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.CustomModels
+{
+    public class DoseSpotPharmacySearch {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+
+    public class DoseSpotPatientEntry
+    {
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
